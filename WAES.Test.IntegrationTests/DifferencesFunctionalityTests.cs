85afb5046ad61550f6d0763a7dce566c556d9e27
Thomas Chavakis <tomchavakis@gmail.com>
2018-01-28 14:02:29 +0200

    * Fixing Previous Commit

---
+++ WAES.Test.IntegrationTests/DifferencesFunctionalityTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using WAES.Client;
+using WAES.Model;
+
+namespace WAES.Test.IntegrationTests
+{
+    [TestFixture]
+    public class DifferencesFunctionalityTests
+    {
+        const string basePath = "http://localhost:5000/api";
+        //private string basePath = "http://localhost:9000/";
+
+
+        /// <summary>
+        /// Add base64 data to the database at specidic id
+        /// </summary>
+        /// <returns></returns>
+        [SetUp]
+        public async Task Setup()
+        {
+            MessageBinding bindingModel = new MessageBinding();
+            bindingModel.Payload = "ZG9jdG9yV2hv"; //doctorWho
+            await DifferencesClient.LefTask(basePath, 1, 15, bindingModel); 
+        }
+        
+        /// <summary>
+        /// Create a POST Action to Test Left Method
+        /// </summary>
+        [Test]
+        public void DifferencesLeftTest()
+        {
+            MessageBinding bindingModel = new MessageBinding();
+            bindingModel.Payload = "ZG9jdG9yV2hp"; //doctorWhi
+            ComparisonResult result = DifferencesClient.LefTask(basePath, 1, 15, bindingModel).Result;
+            Assert.AreEqual(result.AreEqual,ComparisonResultEnum.Equal);
+            Assert.AreEqual(result.Offsets, new int[] {8});
+            Assert.AreEqual(result.OffsetsLength, 1);
+        }
+    }
+}
---
+++ WAES.Tests.UnitTests/CryptographyTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using NUnit.Framework;
+using WAES.BitsConverter;
+using WAES.Model;
+using WAYS.Cryptography;
+
+namespace WAES.Tests.UnitTests
+{
+    [TestFixture]
+    public class CryptographyTests
+    {
+        [Test]
+        [TestCase("U3RyaW5nVG9CYXNlNjRFbmNvZGluZw==")]
+        public void IsValidBase64Test_ValidBase64_ReturnTrue(string message)
+        {
+            bool result = Methods.IsValidBase64(message);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        [TestCase("U3RyaW5nVG9CYXNlNjRFbmNvZGluZ==")]
+        public void IsValidBase64Test_NotValidBase64_ReturnFalse(string message)
+        {
+            bool result = Methods.IsValidBase64(message);
+            Assert.IsFalse(result);
+        }
+
+
+        [Test]
+        [TestCase("StringToBase64Encoding", "U3RyaW5nVG9CYXNlNjRFbmNvZGluZw==")]
+        [TestCase("ThomasChavakis", "VGhvbWFzQ2hhdmFraXM=")]
+        public void Base64Encode_StringHasValue_ReturnBase64(string encodingMessage, string expectedResult)
+        {
+            string result = Methods.Base64EncodeText(encodingMessage);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Base64Encode_StringIsEmptyOrNull_ThrowsArguementNullException(string encodingMessage)
+        {
+            Assert.That(() => Methods.Base64EncodeText(encodingMessage), Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        [TestCase("U3RyaW5nVG9CYXNlNjRFbmNvZGluZw==", "StringToBase64Encoding")]
+        [TestCase("VGhvbWFzQ2hhdmFraXM=", "ThomasChavakis")]
+        public void Base64Decode_DecodeEncodingString_ReturnDecodedMessage(string encodingMessage,
+            string expectedResult)
+        {
+            string result = Methods.Base64DecodeText(encodingMessage);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Base64Decode_StringIsEmptyOrNull_ThrowsArguementNullException(string encodingMessage)
+        {
+            Assert.That(() => Methods.Base64DecodeText(encodingMessage), Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        [TestCase("VGhvbWFzQ2hhdmFraXM=",
+            "0101010001101000011011110110110101100001011100110100001101101000011000010111011001100001011010110110100101110011")]
+        public void Base64DecodeToBinaryDataString_DecodeBase64Message_ReturnBinaryStringData(string encodingMessage,
+            string expextedResult)
+        {
+            string result = Methods.Base64DecodeToBinaryDataString(encodingMessage);
+            Assert.That(result, Is.EqualTo(expextedResult));
+        }
+
+        [Test]
+        [TestCase("VGhvbWFzQ2hhdmFraXM=", new byte[] {84, 104, 111, 109, 97, 115, 67, 104, 97, 118, 97, 107, 105, 115})]
+        public void DecodeBase64ToByteArray_DecodeBase64Message_ReturnByteArray(string encodingMessage,
+            byte[] expextedResult)
+        {
+            byte[] result = Methods.DecodeBase64ToByteArray(encodingMessage);
+            Assert.That(result, Is.EqualTo(expextedResult));
+        }
+
+        [Test]
+        [TestCase(new byte[] {84, 104, 111, 109, 97, 115, 67, 104, 97, 118, 97, 107, 105, 115}, new byte[]
+            {84, 104, 111, 109, 97, 115, 67, 104, 97, 118, 97, 107, 105, 115})]
+        public void CompareByteArrays_AreEqual_ReturnZeroDiffs(byte[] a, byte[] b)
+        {
+            ComparisonResult result = BitsDiff.CompareByteArrays(a, b);
+            Assert.AreEqual(result.AreEqual, ComparisonResultEnum.Equal);
+            Assert.AreEqual(result.Offsets.Length, 0);
+            Assert.AreEqual(result.OffsetsLength, 0);
+        }
+
+        [Test]
+        [TestCase(new byte[] {84, 104, 111, 109, 97, 115, 67, 104, 97, 118, 97, 107, 105, 115}, new byte[] {84, 104, 111, 109, 97, 115, 67, 104, 97, 118, 97, 107, 104, 114})]
+        public void CompareByteArrays_AreEqual_Return2Diffs(byte[] a, byte[] b)
+        {
+            ComparisonResult result = BitsDiff.CompareByteArrays(a, b);
+            Assert.AreEqual(result.AreEqual, ComparisonResultEnum.Equal);
+            Assert.AreEqual(result.OffsetsLength, 2);
+            Assert.AreEqual(result.Offsets, new int[] {12, 13});
+        }
+    }
+}
